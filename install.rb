0e16f5871d5ed99abc707f67a29e2a8e60bcb015
Matthew Lyon <matthew@lyonheart.us>
2011-02-16 23:32:45 -0800

    remove obsolete stuff from when this repo was private

--- install.rb
+++ install.rb
@@ -19,11 +19,3 @@ end
 
 %w(env dir_aliases).each {|f| `touch #{home}/.#{f}` }
 `mkdir #{home}/.zsh_cache` unless File.exists?("#{home}/.zsh_cache")
-
-if File.file?("#{home}/.id_dsa") && ! File.exists?('ssh/id_dsa')
-  `ln -s #{home}/.id_dsa ssh/id_dsa`
-end
-
-# git push on commit
-`echo 'git push' > .git/hooks/post-commit`
-`chmod 755 .git/hooks/post-commit`
