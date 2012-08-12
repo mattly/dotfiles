83ce5e3e8385c6dec5057ab0467a2b1735725044
Matthew Lyon <matthew@lyonheart.us>
2012-08-12 10:13:47 -0700

    haven't used dir_aliases in a while

--- install.rb
+++ install.rb
@@ -17,5 +17,5 @@ Dir['*'].each do |file|
   `ln -s #{File.expand_path file} #{target}`
 end
 
-%w(env dir_aliases).each {|f| `touch #{home}/.#{f}` }
+%w(env).each {|f| `touch #{home}/.#{f}` }
 `mkdir #{home}/.zsh_cache` unless File.exists?("#{home}/.zsh_cache")
