83f6bd845044707957c695fa21bc4f621f363e26
Matthew Lyon <matthew@lyonheart.us>
2014-01-19 00:40:34 -0200

    zsh: remove old functions

--- config/fish/functions/dark.fish
+++
@@ -1,3 +0,0 @@
-function dark -d "set background to dark solarized"
-  set -xg COLORFGBG '12;8'
-end
--- config/fish/functions/light.fish
+++
@@ -1,3 +0,0 @@
-function light -d "set background to light solarized"
-  set -gx COLORFGBG '11;15'
-end
