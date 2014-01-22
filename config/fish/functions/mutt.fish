d3bbee8fdc8fece51acec9195f31806a3f770220
Matthew Lyon <matthew@lyonheart.us>
2014-01-22 00:40:55 -0200

    fish: fix mutt function for args

--- config/fish/functions/mutt.fish
+++ config/fish/functions/mutt.fish
@@ -1,5 +1,5 @@
 function mutt
     tmux rename-window "mutt"
-    bash --login -c 'cd ~/Desktop; /usr/local/bin/mutt' $argv;
+    bash --login -c "cd ~/Desktop; /usr/local/bin/mutt $argv";
     tmux rename-window (echo $PWD | sed -e "s|$HOME|~|")
 end
