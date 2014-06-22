8850b463caef17780725afc299e2590a77f48342
Matthew Lyon <matthew@lyonheart.us>
2014-06-21 19:23:38 -0700

    remove old bork

--- install/node.sh
+++
@@ -1,7 +0,0 @@
-github OiNutter/nodenv $HOME/.nodenv
-github OiNutter/node-build $HOME/.nodenv/plugins/node-build
-
-nodenv 0.6.17
-nodenv 0.8.14
-nodenv 0.10.12
-
--- install/ruby.sh
+++
@@ -1,7 +0,0 @@
-brew rbenv
-brew ruby-build
-
-rbenv 1.9.3-p429
-export CONFIGURE_OPTS=--with-openssl-dir=`command brew --prefix openssl`
-rbenv 2.0.0-p195
-
