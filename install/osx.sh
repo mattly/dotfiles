0e3e244b71cccd4a821d9f3372b4343f80191a78
Matthew Lyon <matthew@lyonheart.us>
2014-06-28 11:18:46 -0700

    update osx to new bork

--- install/osx.sh
+++ install/osx.sh
@@ -1,62 +1,62 @@
 #!/usr/bin/env sh
 
 # auto-expand save, print dialogs
-defaults NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+ok defaults NSGlobalDomain NSNavPanelExpandedStateForSaveMode bool true
+ok defaults NSGlobalDomain PMPrintingExpandedStateForPrint bool true
 # default to save to disk, not iCloud
-defaults NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+ok defaults NSGlobalDomain NSDocumentSaveNewDocumentsToCloud bool false
 # speed up the UI
-defaults NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-defaults NSGlobalDomain NSWindowResizeTime .001
+ok defaults NSGlobalDomain NSAutomaticWindowAnimationsEnabled bool false
+ok defaults NSGlobalDomain NSWindowResizeTime string .001
 # fix the UI
-defaults NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+ok defaults NSGlobalDomain AppleEnableMenuBarTransparency bool false
 # show all extensions
-defaults NSGlobalDomain AppleShowAllExtensions -bool true
+ok defaults NSGlobalDomain AppleShowAllExtensions bool true
 # disable auto spelling correction
-defaults NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+ok defaults NSGlobalDomain NSAutomaticSpellingCorrectionEnabled bool false
 
-defaults com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults com.apple.frameworks.diskimages skip-verify -bool true
-defaults com.apple.frameworks.diskimages skip-verify-remote -bool true
-defaults com.apple.frameworks.diskimages skip-verify-locked -bool true
+ok defaults com.apple.desktopservices DSDontWriteNetworkStores bool true
+ok defaults com.apple.frameworks.diskimages skip-verify bool true
+ok defaults com.apple.frameworks.diskimages skip-verify-remote bool true
+ok defaults com.apple.frameworks.diskimages skip-verify-locked bool true
 
-defaults com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+ok defaults com.apple.TimeMachine DoNotOfferNewDisksForBackup bool true
 
-defaults com.apple.dock autohide -bool true
-defaults com.apple.dock static-only -bool true
-defaults com.apple.dock workspaces-swoosh-animation-off -bool true
-defaults com.apple.dashboard mcx-disabled -bool true
-defaults com.apple.dock tilesize -int 36
+ok defaults com.apple.dock autohide bool true
+ok defaults com.apple.dock static-only bool true
+ok defaults com.apple.dock workspaces-swoosh-animation-off bool true
+ok defaults com.apple.dashboard mcx-disabled bool true
+ok defaults com.apple.dock tilesize integer 36
 # dock show/hide
-defaults com.apple.dock autohide-delay -float 0
-defaults com.apple.dock autohide-time-modifier -float 0
+ok defaults com.apple.dock autohide-delay float 0
+ok defaults com.apple.dock autohide-time-modifier float 0
 # don't re-order spaces in mission control
-defaults com.apple.dock mru-spaces -bool false
+ok defaults com.apple.dock mru-spaces bool false
 # top-left corner: start screen saver
-defaults com.apple.dock wvous-tl-corner -int 5
-defaults com.apple.dock wvous-tl-modifier -int 0
+ok defaults com.apple.dock wvous-tl-corner integer 5
+ok defaults com.apple.dock wvous-tl-modifier integer 0
 # Put it on the left
-defaults com.apple.dock orientation -string left
+ok defaults com.apple.dock orientation string left
 # clear the dock
-rm ~/Library/Application\ Support/Dock/*.db
+# ok rm ~/Library/Application\ Support/Dock/*.db
 
 # DIE DASHBOARD DIE
-defaults com.apple.dashboard mcx-disabled -boolean YES
+ok defaults com.apple.dashboard mcx-disabled bool true
 
 # TODO: if changed...
-killall Dock
+# killall Dock
 
-defaults com.apple.Finder FXPreferredViewStyle clmv
-defaults com.apple.finder FXEnableExtensionChangeWarning -bool false
-defaults com.apple.finder EmptyTrashSecurely -bool true
+ok defaults com.apple.Finder FXPreferredViewStyle string clmv
+ok defaults com.apple.finder FXEnableExtensionChangeWarning bool false
+# ok defaults com.apple.finder EmptyTrashSecurely bool true
 # show ~/Library
-chflags nohidden ~/Library
+# chflags nohidden ~/Library
 # TODO: if changed...
-killall Finder
+# killall Finder
 
 # require password immediately on sleep or screensaver
-defaults com.apple.screensaver askForPassword -int 1
-defaults com.apple.screensaver askForPasswordDelay -int 0
+ok defaults com.apple.screensaver askForPassword integer 1
+ok defaults com.apple.screensaver askForPasswordDelay integer 0
 
 
 
