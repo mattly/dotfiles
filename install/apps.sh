e2d5ecfbd475e80addc9d7c1a56217e9de897741
Matthew Lyon <matthew@lyonheart.us>
2014-06-30 13:00:04 -0700

    initial casking

---
+++ install/apps.sh
@@ -0,0 +1,41 @@
+ok cask alfred
+ok cask crashplan
+ok cask dropbox
+ok cask google-drive
+ok cask little-snitch
+ok cask the-unarchiver
+
+# ok cask daisydisk # via appstore
+ok cask onepassword
+
+#ok cask fantastical
+ok cask google-chrome
+ok cask google-hangouts
+ok cask kindle
+# ok cask pocket
+ok cask vlc
+
+ok cask adobe-creative-cloud
+ok cask adobe-photoshop-lightroom
+# ok cask search colorchooser
+ok cask colorpicker-developer
+ok cask gifrocket
+ok cask fontprep
+# ok cask pixelmator
+# ok cask sketch
+# ok cask sketchbook-pro
+# ok cask xscope
+
+# ok cask albeton-live
+ok cask fission
+# ok cask max-msp
+
+# ok cask calca
+ok cask marked
+
+ok cask atom
+# ok cask dash
+# ok cask duo
+ok cask iterm2
+ok cask vagrant
+ok cask virtualbox
