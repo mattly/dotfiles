c6afc3fafba4b8dd214cdbdda183d6c2d3cc5e87
Matthew Lyon <matthew@lyonheart.us>
2014-07-31 17:11:58 -0700

    system-specific stuff

---
+++ install/system.sh
@@ -0,0 +1,21 @@
+echo "Please enter your administrator password:"
+sudo -v
+
+ok check "sudo fdesetup status | grep 'On'"
+if check_failed; then
+  echo "Filevault is not setup.  Please enable filevault before continuing."
+  # exit 1
+fi
+ok scutil ComputerName lluvia
+ok scutil HostName lluvia
+ok scutil LocalHostName lluvia
+
+ok directory "$HOME/.ssh"
+ok check "[ -e $HOME/.ssh/*.pub ]"
+if check_failed && satisfying; then
+  ssh-keygen -t rsa
+fi
+ok check "[ -e $HOME/.ssh/config ]"
+if check_failed; then
+  echo "copy .ssh/config file from 1password"
+fi
