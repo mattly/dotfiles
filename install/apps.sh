2ac114a49faa1e708277eef9f9aabf32a97a2e14
Matthew Lyon <matthew@lyonheart.us>
2014-07-31 17:10:37 -0700

    beef up apps

--- install/apps.sh
+++ install/apps.sh
@@ -1,41 +1,56 @@
+ok brew brew-cask --from=caskroom/cask
+
 ok cask alfred
 ok cask crashplan
 ok cask dropbox
 ok cask google-drive
 ok cask little-snitch
 ok cask the-unarchiver
+ok cask totalfinder
 
-# ok cask daisydisk # via appstore
+ok macstore DaisyDisk 411643860
 ok cask onepassword
+ok cask little-snitch
 
-#ok cask fantastical
+ok macstore Fantastical 435003921
 ok cask google-chrome
 ok cask google-hangouts
 ok cask kindle
-# ok cask pocket
+ok macstore Pocket 568494494
 ok cask vlc
+ok macstore Tweetbot 557168941
+ok macstore Reeder 880001334
 
 ok cask adobe-creative-cloud
 ok cask adobe-photoshop-lightroom
-# ok cask search colorchooser
+ok macstore ColorChooser 445279824
+ok macstore Pixelmator 407963104
+ok macstore Sketch 852320343
+ok macstore SketchBookPro6 553377898
+ok macstore xScope 889428659
 ok cask colorpicker-developer
 ok cask gifrocket
 ok cask fontprep
-# ok cask pixelmator
-# ok cask sketch
-# ok cask sketchbook-pro
-# ok cask xscope
 
 # ok cask albeton-live
+ok check "[ -d '/Applications/Albeton Live 9 Standard.app' ]"
 ok cask fission
 ok cask cycling74-max
+ok macstore Capo 696977615
+ok check "[ -d '/Applications/Chromaphone.app' ]"
+ok check "[ -d '/Applications/Lounge Lizard EP-4.app' ]"
+ok check "[ -d '/Applications/Native Instruments' ]"
+ok check "[ -d '/Applications/String Studio.app' ]"
+ok check "[ -d '/Applications/Ultra Analog VA-2.app' ]"
+ok check "[ -d '/Library/Audio/Plug-ins/VST/Audio Damage' ]"
 
-# ok cask calca
+ok macstore Calca 635758264
 ok cask marked
 
 ok cask atom
-# ok cask dash
-# ok cask duo
+ok macstore Dash 458034879
+ok macstore Duo 777886035
+ok macstore Blind 687450044
 ok cask iterm2
 ok cask vagrant
 ok cask virtualbox
