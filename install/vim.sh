cc9ab02f8b836f9a9bfe722a6aaa55f6332b6f22
Matthew Lyon <matthew@lyonheart.us>
2014-09-03 22:59:20 -0700

    vim: +jsx, +gabrielelana/vim-markdown

--- install/vim.sh
+++ install/vim.sh
@@ -40,7 +40,7 @@ destination $code/dotfiles/configs/vim/bundle
   ok github reedes/vim-pencil
   ok github reedes/vim-lexical
   ok github reedes/vim-wordy
-  ok github nelstrom/vim-markdown-folding
+  ok github gabrielelana/vim-markdown
 
 # outlining
   ok github vim-voom/VOoM
@@ -58,59 +58,60 @@ destination $code/dotfiles/configs/vim/bundle
   ok github kien/rainbow_parentheses.vim
 
 # git & scm
-  ok github tpope/vim-git                 # .gitcommit
+  ok github tpope/vim-git                   # .gitcommit
   ok github tpope/vim-fugitive
   ok github mhinz/vim-signify
   ok github idanarye/vim-merginal
 
 # Language/Environment-Specific
 
-  # Clojure
-    ok github guns/vim-clojure-static       # .clj        clojure syntax
-    ok github guns/vim-sexp
-    ok github tpope/vim-sexp-mappings-for-regular-people
-    ok github tpope/vim-fireplace
+# Clojure
+  ok github guns/vim-clojure-static       # .clj        clojure syntax
+  ok github guns/vim-sexp
+  ok github tpope/vim-sexp-mappings-for-regular-people
+  ok github tpope/vim-fireplace
 
-  # CSS
-    ok github cakebaker/scss-syntax.vim     # .scss       -> .css
-    ok github groenewege/vim-less           # .less       -> .css
-    ok github wavded/vim-stylus             # .styl       -> .css
+# CSS
+  ok github cakebaker/scss-syntax.vim     # .scss       -> .css
+  ok github groenewege/vim-less           # .less       -> .css
+  ok github wavded/vim-stylus             # .styl       -> .css
 
-  # Javascript
-    ok github pangloss/vim-javascript       # .js         better indenting
-    ok github kchmck/vim-coffee-script      # .coffee     -> .js
-    ok github mintplant/vim-literate-coffeescript
-                                        # .litcoffee  -> .js
-    ok github lambdatoast/elm.vim           # .elm
+# Javascript
+  ok github pangloss/vim-javascript       # .js         better indenting
+  ok github kchmck/vim-coffee-script      # .coffee     -> .js
+  ok github mintplant/vim-literate-coffeescript
+                                          # .litcoffee  -> .js
+  ok github lambdatoast/elm.vim           # .elm
+  ok github mxw/vim-jsx                   # React's JSX, uses .js with a pragma
 
-  # Markup / Templates
-    ok github tpope/vim-ragtag              # .html       formatting tools
-    ok github juvenn/mustache               # .mustache   :{
-    ok github Glench/Vim-Jinja2-Syntax      # .html       jinja/nunjukcs/swig
-    ok github digitaltoad/vim-jade          # .jade       -> .html
+# Markup / Templates
+  ok github tpope/vim-ragtag              # .html       formatting tools
+  ok github juvenn/mustache               # .mustache   :{
+  ok github Glench/Vim-Jinja2-Syntax      # .html       jinja/nunjukcs/swig
+  ok github digitaltoad/vim-jade          # .jade       -> .html
 
-  # Shell
-    ok github rosstimson/bats.vim           # .bats       bash unit testing
-    ok github aliva/vim-fish                # .fish
+# Shell
+  ok github rosstimson/bats.vim           # .bats       bash unit testing
+  ok github aliva/vim-fish                # .fish
 
-  # Viml
-    ok github tpope/vim-scriptease
-    ok github dbakker/vim-lint
+# Viml
+  ok github tpope/vim-scriptease
+  ok github dbakker/vim-lint
 
-  # Miscellaneous
-    ok github vim-scripts/csv.vim           # .csv
-    ok github ekalinin/Dockerfile.vim       # Dockerfile
-    ok github elixir-lang/vim-elixir        # .ex
-    ok github jimenezrick/vimerl            # .erl
-    ok github jnwhiteh/vim-golang           # .go
-    ok github wannesm/wmgraphviz.vim        # .gv         graphviz
-    ok github travitch/hasksyn              # .hs
-    ok github wting/rust.vim                # .rust
-    ok github toyamarinyon/vim-swift
-    ok github exu/pgsql.vim                 # .sql        postgresql 4 life
-    ok github andersoncustodio/vim-tmux     # tmux.config
-    ok github sheerun/vim-yardoc            # yard inside .rb
+# Miscellaneous
+  ok github vim-scripts/csv.vim           # .csv
+  ok github ekalinin/Dockerfile.vim       # Dockerfile
+  ok github elixir-lang/vim-elixir        # .ex
+  ok github jimenezrick/vimerl            # .erl
+  ok github jnwhiteh/vim-golang           # .go
+  ok github wannesm/wmgraphviz.vim        # .gv         graphviz
+  ok github travitch/hasksyn              # .hs
+  ok github wting/rust.vim                # .rust
+  ok github toyamarinyon/vim-swift
+  ok github exu/pgsql.vim                 # .sql        postgresql 4 life
+  ok github andersoncustodio/vim-tmux     # tmux.config
+  ok github sheerun/vim-yardoc            # yard inside .rb
 
 if any_updated; then
-  vim -c ':Helptags'
+  vim -c ':Helptags' -c ':q'
 fi
