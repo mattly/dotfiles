ca3bdac17110e46f0d8d9d6822d1f9a483680c28
Matthew Lyon <matthew@lyonheart.us>
2014-10-29 11:41:50 -0400

    cleanup apps.sh

--- install/apps.sh
+++ install/apps.sh
@@ -1,7 +1,6 @@
 ok brew brew-cask --from=caskroom/cask
 
 ok cask alfred
-ok cask crashplan
 ok cask dropbox
 ok cask google-drive
 ok cask little-snitch
@@ -15,14 +14,12 @@ ok cask little-snitch
 ok macstore Fantastical 435003921
 ok cask google-chrome
 ok cask google-hangouts
-ok cask kindle
 ok macstore Pocket 568494494
 ok cask vlc
 ok macstore Tweetbot 557168941
 ok macstore Reeder 880001334
 
 ok cask adobe-creative-cloud
-ok cask adobe-photoshop-lightroom
 ok macstore ColorChooser 445279824
 ok macstore Pixelmator 407963104
 ok macstore Sketch 852320343
@@ -33,14 +30,14 @@ ok cask gifrocket
 ok cask fontprep
 
 # ok cask albeton-live
-ok check "[ -d '/Applications/Albeton Live 9 Standard.app' ]"
+ok check "[ -d '/Applications/Ableton Live 9 Standard.app' ]"
 ok cask fission
 ok cask cycling74-max
 ok macstore Capo 696977615
 ok check "[ -d '/Applications/Chromaphone.app' ]"
 ok check "[ -d '/Applications/Lounge Lizard EP-4.app' ]"
 ok check "[ -d '/Applications/Native Instruments' ]"
-ok check "[ -d '/Applications/String Studio.app' ]"
+ok check "[ -d '/Applications/String Studio VS-2.app' ]"
 ok check "[ -d '/Applications/Ultra Analog VA-2.app' ]"
 ok check "[ -d '/Library/Audio/Plug-ins/VST/Audio Damage' ]"
 
