4cdde9dae27ef4982afb597e4c3b832f75002519
Matthew Lyon <matthew@lyonheart.us>
2015-04-24 17:24:26 -0700

    spacemacs tweaks

--- configs/spacemacs-layers/prose/packages.el
+++ configs/spacemacs-layers/prose/packages.el
@@ -5,7 +5,11 @@
                          ;; minor modes
                          cm-mode
                          olivetti
-                         writeroom-mode))
+                         writeroom-mode
+
+                         ;; etc
+                         emacs-sentence-navigation
+                         ))
 
 (defvar prose-excluded-packages '())
 
@@ -46,5 +50,15 @@
       (evil-leader/set-key
         "tW" 'writeroom-mode))))
 
-
+(defun prose/emacs-sentence-navigation ()
+  (use-package emacs-sentence-navigation
+    :defer t
+    :config
+    (progn
+      (define-key evil-normal-state-map ")" 'sn/evil-forward-sentence)
+      (define-key evil-normal-state-map "(" 'sn/evil-backward-sentence)
+      (define-key evil-normal-state-map "g)" 'sn/evil-forward-sentence-end)
+      (define-key evil-normal-state-map "g(" 'sn/evil-backward-sentence-end)
+      (define-key evil-outer-text-objects-map "S" 'sn/evil-outer-sentence)
+      (define-key evil-inner-text-objects-map "S" 'sn/evil-inner-sentence))))
 
