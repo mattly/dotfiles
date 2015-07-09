c627022787415a8be63f64eda9ef2cdaed8719d7
Matthew Lyon <matthew@lyonheart.us>
2015-07-09 10:28:35 -0700

    add local lein config

---
+++ configs/lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.10"]]}}
