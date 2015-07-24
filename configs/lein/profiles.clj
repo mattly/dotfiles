3b36d44620ab954fdb76e70c5e90a298c87959c6
Matthew Lyon <matthew@lyonheart.us>
2015-07-24 16:25:59 -0700

    New lein repl stuff
    From this excellent article: from
    http://dev.solita.fi/2014/03/18/pimp-my-repl.html

--- configs/lein/profiles.clj
+++ configs/lein/profiles.clj
@@ -1,4 +1,17 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [refactor-nrepl "1.1.0"]]
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.10"]]}}
+                       [org.clojure/tools.nrepl "0.2.10"]
+                       [org.clojars.gjahad/debug-repl "0.3.3"]
+                       [im.chit/vinyasa "0.3.4"]
+                       [spyscope "0.1.5"]
+                       [difform "1.1.2"]
+                       [clj-ns-browser "1.4.0-SNAPSHOT"]]
+        :injections [(require 'vinyasa.inject)
+                     (require 'alex-and-georges.debug-repl)
+                     (vinyasa.inject/inject 'clojure.core '>
+                                            '[[clojure.repl doc source]
+                                              [clojure.pprint pprint pp]
+                                              [alex-and-georges.debug-repl debug-repl]
+                                              [com.georgejahad.difform difform]
+                                              [clj-ns-browser.sdoc sdoc]])]}}
