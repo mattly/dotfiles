790e0e3ae8b9ddc05dbeafadb08119d98829ee15
Matthew Lyon <matthew@lyonheart.us>
2015-07-24 17:15:21 -0700

    Fix the vinyasa config based on its readme

--- configs/lein/profiles.clj
+++ configs/lein/profiles.clj
@@ -3,15 +3,28 @@
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.10"]
                        [org.clojars.gjahad/debug-repl "0.3.3"]
+                       [org.clojure/tools.namespace "0.2.4"]
+                       [leiningen #=(leiningen.core.main/leiningen-version)]
+                       [io.aviso/pretty "0.1.8"]
                        [im.chit/vinyasa "0.3.4"]
                        [spyscope "0.1.5"]
-                       [difform "1.1.2"]
-                       [clj-ns-browser "1.4.0-SNAPSHOT"]]
-        :injections [(require 'vinyasa.inject)
+                       [difform "1.1.2"]]
+        :injections [(require 'spyscope.core)
+                     (require '[vinyasa.inject :as inject])
                      (require 'alex-and-georges.debug-repl)
-                     (vinyasa.inject/inject 'clojure.core '>
-                                            '[[clojure.repl doc source]
-                                              [clojure.pprint pprint pp]
-                                              [alex-and-georges.debug-repl debug-repl]
-                                              [com.georgejahad.difform difform]
-                                              [clj-ns-browser.sdoc sdoc]])]}}
+                     (inject/in [vinyasa.inject :refer [inject [in inject-in]]]
+                                [vinyasa.lein :exclude [*project*]]
+                                [vinyasa.pull :all]
+
+                                ;; inject into clojure.core
+                                clojure.core
+                                [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
+
+                                ;; inject into clojure.core with prefix
+                                clojure.core >
+                                [clojure.repl doc source]
+                                [clojure.pprint pprint pp]
+                                [clojure.java.shell sh]
+                                [alex-and-georges.debug-repl debug-repl]
+                                [com.georgejahad.difform difform]
+                                )]}}
