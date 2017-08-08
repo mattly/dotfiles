01fecfe8a297c70bdb1644a1939ec899cbe4ea10
Matthew Lyon <matthew@lyonheart.us>
2017-08-08 09:23:12 -0700

    Change hyper theme to a light one

--- configs/hyper.js
+++ configs/hyper.js
@@ -116,7 +116,13 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-bloody", "hyper-tabs-enhanced", "hyper-monokai", "hypercwd"],
+  plugins: [
+    "hyper-bloody",
+    "hyper-tabs-enhanced",
+    "hyper-monokai",
+    "hypercwd",
+    "hyper-ayu-light"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
