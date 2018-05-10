e38f1c9b6ad03623d89896c4a886bc1fb8d0e0cd
Matthew Lyon <matthew@lyonheart.us>
2018-05-10 14:11:27 -0700

    fish: remove mutt wrapper function
    I haven't used mutt in *years*

--- configs/config/fish/functions/mutt.fish
+++
@@ -1,8 +0,0 @@
-function mutt
-    set window_name (tmux list-windows | grep '(active)$' | awk '{print $2}' | sed -E 's|\*$||')
-    tmux rename-window "mutt"
-    pushd ~/Desktop
-    /usr/local/bin/mutt $argv
-    popd
-    tmux rename-window $window_name
-end
