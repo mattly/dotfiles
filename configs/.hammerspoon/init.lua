7fd9a13ac9e19dbb307efa1f725225c1cd3382dd
Matthew Lyon <matthew@lyonheart.us>
2018-06-12 06:56:32 -0700

    Trying out the space cadet keyboard w/Karabiner

--- configs/.hammerspoon/init.lua
+++ configs/.hammerspoon/init.lua
@@ -1,5 +1,5 @@
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Left", function ()
     local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
@@ -7,34 +7,22 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
 
     f.x = max.x
     f.y = max.y
-    f.w = max.w
-    f.h = max.h / 2
+    f.w = max.w / 2
+    f.h = max.h
     win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Right", function ()
     local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
     local max = screen:frame()
 
-    f.x = max.x
-    f.y = max.y + max.h/2
-    f.w = max.w
-    f.h = max.h / 2
+    f.x = max.x + max.w/2
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
     win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-    local win = hs.window.focusedWindow()
-    local screen = win:screen()
-    local rotation = screen:rotate()
-
-    if rotation == 0 then
-      screen:rotate(90)
-    else
-      screen:rotate(0)
-    end
 end)
 
 
@@ -50,5 +38,4 @@ function reloadConfig(files)
   end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.loadSpoon('ControlEscape'):start()
 hs.alert.show("Config loaded")
