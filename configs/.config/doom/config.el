c63e257f8baa8df1bba539810db3e5d38b6bb922
Matthew Lyon <matthew@lyonheart.us>
2018-06-23 07:17:19 -0700

    DOOM: folding magic from @hlissner for ruby/yaml

--- configs/.config/doom/config.el
+++ configs/.config/doom/config.el
@@ -22,3 +22,22 @@
   :defer t
   :config
   (setq feature-step-search-path "features/**/*steps.rb"))
+
+
+(defun +data-hideshow-forward-sexp (arg)
+  (let ((start (current-indentation)))
+    (forward-line)
+    (unless (= start (current-indentation))
+      (require 'evil-indent-plus)
+      (let ((range (evil-indent-plus--same-indent-range)))
+        (goto-char (cadr range))
+        (end-of-line)))))
+
+(map-put hs-special-modes-alist
+         'yaml-mode
+         '("\\s-*\\_<\\(?:[^:]+\\)\\_>" "" "#" +data-hideshow-forward-sexp nil))
+
+(map-put hs-special-modes-alist
+         'ruby-mode
+         '("\\(class\\|module\\|def\\|do\\|{\\)" "\\(end\\|end\\|}\\)" "#"
+           (lambda (arg) (ruby-end-of-block)) nil))
