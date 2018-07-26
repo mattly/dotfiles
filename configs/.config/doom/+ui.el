13f3c1917bde722078bf518287fbe649b58fd68a
Matthew Lyon <matthew@lyonheart.us>
2018-07-26 10:09:45 -0700

    DOOM: bump font sizes

--- configs/.config/doom/+ui.el
+++ configs/.config/doom/+ui.el
@@ -2,9 +2,9 @@
 
 ;; fonts
 (setq text-scale-mode-step 1.05
-      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 14)
-      doom-font (font-spec :family "IBM Plex Mono" :size 14 :weight 'light)
-      doom-big-font (font-spec :family "IBM Plex Mono" :size 18 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 16)
+      doom-font (font-spec :family "IBM Plex Mono" :size 16 :weight 'light)
+      doom-big-font (font-spec :family "IBM Plex Mono" :size 21 :weight 'light)
       doom-themes-enable-bold t
       doom-themes-enable-italic t)
 
@@ -27,6 +27,9 @@
       ;; -- lighter
       ;; 'doom-one-light
       ;; 'doom-alabaster
+      doom-challenger-deep-brighter-comments t
+      doom-challenger-deep-brighter-modeline t
+      doom-challenger-deep-padded-modeline nil
       doom-dracula-brighter-comments t
       doom-dracula-brighter-modeline t
       doom-dracula-colorful-headers t
