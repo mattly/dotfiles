6c639e4501e32ed195d2273e03ddcdea0cc8caca
Matthew Lyon <matthew@lyonheart.us>
2018-08-21 08:34:25 -0700

    DOOM: back to ivy

--- configs/.config/doom/init.el
+++ configs/.config/doom/init.el
@@ -19,13 +19,13 @@
        (company          ; the ultimate code completion backend
         +childframe
         +auto)            ; as-you-type code completion
-       (helm             ; the *other* search engine for love and life
-        +childframe
-        +fuzzy)          ; enable fuzzy search backend for helm
-      ;ido               ; the other *other* search engine...
-      ;(ivy              ; a search engine for love and life
+      ;(helm             ; the *other* search engine for love and life
       ; +childframe
-      ; +fuzzy)           ; enable fuzzy search backend for ivy
+      ; +fuzzy)          ; enable fuzzy search backend for helm
+      ;ido               ; the other *other* search engine...
+       (ivy              ; a search engine for love and life
+        +childframe
+        +fuzzy)           ; enable fuzzy search backend for ivy
 
        :ui
        doom              ; what makes DOOM look the way it does
