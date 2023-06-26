9bdef9117707d42b2dab0d54192c4c3f272f1792
Matthew Lyon <matthew@lyonheart.us>
2023-06-26 10:52:40 -0700

    fish, etc: some cleanup

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -24,51 +24,78 @@ set -g fish_color_cancel normal --bold
 set -g fish_color_valid_path normal --underline
 
 test -e {$HOME}/.iterm2_shell_integration.fish
-and source {$HOME}/.iterm2_shell_integration.fish
+    and source {$HOME}/.iterm2_shell_integration.fish
 
 test -e {$HOME}/.local.fish
-and source {$HOME}/.local.fish
+    and source {$HOME}/.local.fish
 
-which most > /dev/null
-if test $status -eq 0
-    set -x PAGER most
+test (which most)
+    and set -x PAGER most
+
+# prefer zed, fall back to vscode
+test (which code)
+    and set -x EDITOR code
+test (which zed)
+    and set -x EDITOR zed
+
+# various cli utils
+
+test (which bat)
+    and alias cat="bat"
+
+if test (which exa)
+    set -l long "--long --header --classify --modified --time-style long-iso --git --color-scale"
+    alias l="exa --classify --git-ignore "
+    alias ll="exa $long --git-ignore "
+    alias la="exa $long --all "
+    alias lna="exa $long --all --sort=newest "
+    alias ls="exa"
 end
 
-alias ping='prettyping --nolegend'
+test (which prettyping)
+    and alias ping='prettyping --nolegend'
+
+test (which lazygit)
+    and alias lg='lazygit'
+
 set -x PATH ~/projects/dotfiles/bin $PATH
 
 test -e ~/.local/bin
-and set -x PATH ~/.local/bin $PATH
+    and set -x PATH ~/.local/bin $PATH
 
 test -e /opt/homebrew/bin
-and fish_add_path /opt/homebrew/bin
+    and fish_add_path /opt/homebrew/bin
 
 test (which brew)
-and set -x HOMEBREW_NO_AUTO_UPDATE no
+    and set -x HOMEBREW_NO_AUTO_UPDATE no
 
 test (which gpg)
-and set -x GPG_TTY (tty)
+    and set -x GPG_TTY (tty)
 
 # language-specific stuff
 # =========================================================
+
+# --- bun
+if test (which bun)
+    set --export BUN_INSTALL "$HOME/.bun"
+    set --export PATH $BUN_INSTALL/bin $PATH
+end
+
 # --- go
 set -x GOPATH ~/projects/golang
 set -x PATH $GOPATH/bin $PATH
 
 # --- ruby
 test -e /usr/local/opt/ruby/bin
-and set -x PATH /usr/local/opt/ruby/bin $PATH
+    and set -x PATH /usr/local/opt/ruby/bin $PATH
 
 test -e /usr/local/lib/ruby/gems/2.6.0/bin
-and set -x PATH /usr/local/lib/ruby/gems/2.6.0/bin $PATH
+    and set -x PATH /usr/local/lib/ruby/gems/2.6.0/bin $PATH
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
 
-# rust
+# --- rust
 test -e ~/.cargo/bin
-and set -x PATH ~/.cargo/bin $PATH
+    and set -x PATH ~/.cargo/bin $PATH
 
 # =========================================================
 
